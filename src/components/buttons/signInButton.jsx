57e88d549dbd528b7e8efd7030bc9ed29a253c01
galadrielloves2code <deryakarademir2@gmail.com>
2023-06-21 17:21:32 +0300

    bootstrap installed & sign in button created

---
+++ src/components/buttons/signInButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Home } from "../../pages/home";
+
+export function SignInButton() {
+  return (
+    <div>
+      <link to="/home">
+        <button type="button" className="btn btn-primary">
+          Sign in
+        </button>
+      </link>
+    </div>
+  );
+}
