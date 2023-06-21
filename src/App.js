21eee5aee658bfd9254668d6246876ad1971add4
galadrielloves2code <126419291+galadrielloves2code@users.noreply.github.com>
2023-06-21 17:22:26 +0300

    bootstrap installed & sign in button created (#7)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { Home } from './pages/home';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <Home/>
     </div>
   );
 }
