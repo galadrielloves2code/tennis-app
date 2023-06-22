6a05a09f5e37e3453214b1dbc4da71d8c0a5b9cd
galadrielloves2code <deryakarademir2@gmail.com>
2023-06-22 15:00:21 +0300

    gmail button & sign in button created

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { GmailButton } from "../components/buttons/gmailButton";
 
-export function Home (){
-    return(
-        <div>
-            <p>hello</p>
-        </div>
-    )
+export function Home() {
+  return (
+    <div>
+      <p>hello</p>
+      <GmailButton/>
+    </div>
+  );
 }
