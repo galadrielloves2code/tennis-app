f56bdd1d11e4d071e191ee2e4bca9688058cc45d
galadrielloves2code <deryakarademir2@gmail.com>
2023-06-23 14:57:46 +0300

    sign in page is created

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { SignInPage } from "./signInPage";
 
 export function Home() {
   return (
     <div>
       <p>hello</p>
+      <SignInPage/>
     </div>
   );
 }
---
+++ src/pages/signInPage.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import { SignInButton } from "../components/buttons/signInButton";
+export function SignInPage() {
+  return (
+    <Router>
+      <div className="container-sm-center">
+        <img src="../components/imgages/profile.svg" alt="Profile Icon" />
+        <form class="needs-validation" novalidate>
+          <div class="col-md-6 mb-3" className="form-group">
+            <label htmlFor="exampleInputEmail1"></label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Username"
+              required
+            />
+          </div>
+          <div class="col-md-6 mb-3" className="form-group">
+            <label htmlFor="exampleInputPassword1"></label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+              required
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              Choose a password with at least 8 characters, including uppercase
+              letters, lowercase letters, numbers, and symbols
+            </small>
+          </div>
+          <br />
+          <div className="form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+              Remember me
+            </label>
+          </div>
+          <br />
+          <SignInButton />
+        </form>
+        <br />
+        <Link
+          to="/"
+          style={{ textDecoration: "none", color: "black" }}
+          onMouseEnter={(e) => (e.target.style.color = "green")}
+          onMouseLeave={(e) => (e.target.style.color = "black")}
+        >
+          Create an account
+        </Link>
+      </div>
+    </Router>
+  );
+}
