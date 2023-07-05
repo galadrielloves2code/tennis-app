cf3d174829d8bb69510bb99ae51b4f5c76a67b84
galadrielloves2code <126419291+galadrielloves2code@users.noreply.github.com>
2023-07-05 11:05:09 +0300

    twitter and instagram buttons added (#17)

---
+++ src/components/buttons/twitterButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+
+export function TwitterButton() {
+  return <FontAwesomeIcon icon={faTwitter} />;
+}
