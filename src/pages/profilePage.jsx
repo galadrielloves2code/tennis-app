155546bff78b95d0e3bcb325a66581a21289d444
galadrielloves2code <deryakarademir2@gmail.com>
2023-07-11 12:07:35 +0300

    structure of profile page was created

--- src/pages/profilePage.jsx
+++ src/pages/profilePage.jsx
@@ -2,44 +2,62 @@ import React from "react";
 import Flag from "../components/flag";
 import { PlayerPictures } from "../components/playersPictures";
 import { LikeButton } from "../components/buttons/likeButton";
+import { InstagramButton } from "../components/buttons/instagramButton";
+import { TwitterButton } from "../components/buttons/twitterButton";
 
 export function Profile() {
   return (
-    <div style={{ display: "flex" }}>
-      <div
-        className="container"
-        style={{ border: "1px solid black", padding: "10px" }}
-      >
+    <div>
+      {" "}
+      {/*div for entire page*/}
+      <div style={{ display: "flex" }}>
+        {" "}
+        {/* div for the top part of the page including summary of player info and picture */}
         <div
           className="container"
-          style={{
-            border: "1px solid black",
-            padding: "10px",
-            display: "flex",
-          }}
+          style={{ border: "1px solid black", padding: "10px" }}
         >
-          <Flag countryCode="italy" altText="Italy Flag" />
-          <p style={{ marginLeft: "10px" }}>Jannik Sinner</p>
-
-          <div style={{ display: "block", marginLeft: "auto" }}>
-            <h1
+          {" "}
+          {/*div for top left including just the summary of the player and ranking*/}
+          <div
+            className="container"
+            style={{
+              border: "1px solid black",
+              padding: "10px",
+            }}
+          >
+            {" "}
+            {/*div for top left including just the summary of the player and WITHOUT ranking */}
+            <div style={{display:"flex"}}>
+              {" "}
+              {/* flag + player name */}
+              <Flag countryCode="italy" altText="Italy Flag" />
+              <p style={{ marginLeft: "10px" }}>Jannik Sinner</p>
+            </div>
+            <div
+              className="container"
               style={{
                 border: "1px solid black",
-                padding: "20px",
-                borderRadius: "20px",
-                textAlign: "center",
+                display: "flex",
               }}
             >
-              8
-            </h1>
-            <p>Singles Rank</p>
+              {" "}
+              {/* div for social media buttons */}
+              <InstagramButton />
+              <TwitterButton />
+            </div>
+            <div> {/* div for summary info for the player */}
+            
+            </div>
           </div>
+          <div style={{ display: "flex" }}> {/* div for ranking */}</div>
         </div>
-        <div style={{ display: "flex" }}></div>
-      </div>
-      <div style={{ position: "relative" }}>
-        <PlayerPictures playerLastName="sinner" />
-        <LikeButton />
+        <div style={{ position: "relative" }}>
+          {" "}
+          {/* div for top right of the page including just profile picture and the like button on it */}
+          <PlayerPictures playerLastName="sinner" />
+          <LikeButton />
+        </div>
       </div>
     </div>
   );
