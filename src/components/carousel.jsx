7e831c92f3ea009a8dde0efcad14151fe1087290
galadrielloves2code <deryakarademir2@gmail.com>
2023-07-14 14:27:54 +0300

    carousel component was added

---
+++ src/components/carousel.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+
+export function Carousel() {
+  return (
+    <div style={{ height: "373px", width:"724px" }}>
+      <div id="carouselHome" className="carousel slide">
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselHome"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselHome"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselHome"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src={process.env.PUBLIC_URL + "/assets/slides/slide1.jpeg"}
+              className="d-block w-100"
+              alt="Slide 1"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src={process.env.PUBLIC_URL + "/assets/slides/slide2.jpeg"}
+              className="d-block w-100"
+              alt="Slide 2"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src={process.env.PUBLIC_URL + "/assets/slides/slide3.jpeg"}
+              className="d-block w-100"
+              alt="Slide 3"
+            />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselHome"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselHome"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  );
+}
