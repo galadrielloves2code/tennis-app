f68030b7d252dc954a30f1f0f38475b268bdc491
galadrielloves2code <126419291+galadrielloves2code@users.noreply.github.com>
2023-07-17 15:50:11 +0300

    players page was created (#35)

--- src/components/playersCard.jsx
+++ src/components/playersCard.jsx
@@ -2,42 +2,320 @@ import React from "react";
 
 export function PlayerCard() {
   return (
-    <div
-      className="card"
-      style={{
-        width: "12rem",
-        height: "20rem",
-        borderRadius: "24.557px",
-        boxShadow:
-          "0px 13.9154634475708px 13.096906661987305px 0px rgba(0, 0, 0, 0.25)",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <img
-        src="../assets/playersIcons/djokovic.png"
-        className="card-img-top"
-        alt="player picture"
-        style={{ height: "15rem", width: "10rem", marginTop: "30px" }}
-      />
-      <div className="card-body">
-        <a
-          href="#"
-          className="btn"
+    <div>
+      <div
+        className="container"
+        style={{ display: "flex", justifyContent: "space-evenly", marginTop:"20px", marginBottom:"50px" }}
+      >
+        <div
+          className="card"
           style={{
             width: "12rem",
-            height: "5rem",
-            backgroundColor: "#F2F1F1",
-            borderRadius: "0 0 25px 25px ",
+            height: "20rem",
+            borderRadius: "24.557px",
+            boxShadow:
+              "0px 13.9154634475708px 13.096906661987305px 0px rgba(0, 0, 0, 0.25)",
             display: "flex",
-            alignItems: "center",
+            flexDirection: "column",
             justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          Player Name
-        </a>
+          <img
+            src="../assets/playersIcons/2.png"
+            className="card-img-top"
+            alt="player picture"
+            style={{ height: "15rem", width: "10rem", marginTop: "30px" }}
+          />
+          <div className="card-body">
+            <a
+              href="#"
+              className="btn"
+              style={{
+                width: "12rem",
+                height: "5rem",
+                backgroundColor: "#F2F1F1",
+                borderRadius: "0 0 25px 25px ",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              Player Name
+            </a>
+          </div>
+        </div>
+        <div
+          className="card"
+          style={{
+            width: "12rem",
+            height: "20rem",
+            borderRadius: "24.557px",
+            boxShadow:
+              "0px 13.9154634475708px 13.096906661987305px 0px rgba(0, 0, 0, 0.25)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src="../assets/playersIcons/2.png"
+            className="card-img-top"
+            alt="player picture"
+            style={{ height: "15rem", width: "10rem", marginTop: "30px" }}
+          />
+          <div className="card-body">
+            <a
+              href="#"
+              className="btn"
+              style={{
+                width: "12rem",
+                height: "5rem",
+                backgroundColor: "#F2F1F1",
+                borderRadius: "0 0 25px 25px ",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              Player Name
+            </a>
+          </div>
+        </div>
+        <div
+          className="card"
+          style={{
+            width: "12rem",
+            height: "20rem",
+            borderRadius: "24.557px",
+            boxShadow:
+              "0px 13.9154634475708px 13.096906661987305px 0px rgba(0, 0, 0, 0.25)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src="../assets/playersIcons/2.png"
+            className="card-img-top"
+            alt="player picture"
+            style={{ height: "15rem", width: "10rem", marginTop: "30px" }}
+          />
+          <div className="card-body">
+            <a
+              href="#"
+              className="btn"
+              style={{
+                width: "12rem",
+                height: "5rem",
+                backgroundColor: "#F2F1F1",
+                borderRadius: "0 0 25px 25px ",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              Player Name
+            </a>
+          </div>
+        </div>
+        <div
+          className="card"
+          style={{
+            width: "12rem",
+            height: "20rem",
+            borderRadius: "24.557px",
+            boxShadow:
+              "0px 13.9154634475708px 13.096906661987305px 0px rgba(0, 0, 0, 0.25)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src="../assets/playersIcons/2.png"
+            className="card-img-top"
+            alt="player picture"
+            style={{ height: "15rem", width: "10rem", marginTop: "30px" }}
+          />
+          <div className="card-body">
+            <a
+              href="#"
+              className="btn"
+              style={{
+                width: "12rem",
+                height: "5rem",
+                backgroundColor: "#F2F1F1",
+                borderRadius: "0 0 25px 25px ",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              Player Name
+            </a>
+          </div>
+        </div>
+      </div>
+      <div
+        className="container"
+        style={{ display: "flex", justifyContent: "space-evenly" }}
+      >
+        <div
+          className="card"
+          style={{
+            width: "12rem",
+            height: "20rem",
+            borderRadius: "24.557px",
+            boxShadow:
+              "0px 13.9154634475708px 13.096906661987305px 0px rgba(0, 0, 0, 0.25)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src="../assets/playersIcons/2.png"
+            className="card-img-top"
+            alt="player picture"
+            style={{ height: "15rem", width: "10rem", marginTop: "30px" }}
+          />
+          <div className="card-body">
+            <a
+              href="#"
+              className="btn"
+              style={{
+                width: "12rem",
+                height: "5rem",
+                backgroundColor: "#F2F1F1",
+                borderRadius: "0 0 25px 25px ",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              Player Name
+            </a>
+          </div>
+        </div>
+        <div
+          className="card"
+          style={{
+            width: "12rem",
+            height: "20rem",
+            borderRadius: "24.557px",
+            boxShadow:
+              "0px 13.9154634475708px 13.096906661987305px 0px rgba(0, 0, 0, 0.25)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src="../assets/playersIcons/2.png"
+            className="card-img-top"
+            alt="player picture"
+            style={{ height: "15rem", width: "10rem", marginTop: "30px" }}
+          />
+          <div className="card-body">
+            <a
+              href="#"
+              className="btn"
+              style={{
+                width: "12rem",
+                height: "5rem",
+                backgroundColor: "#F2F1F1",
+                borderRadius: "0 0 25px 25px ",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              Player Name
+            </a>
+          </div>
+        </div>
+        <div
+          className="card"
+          style={{
+            width: "12rem",
+            height: "20rem",
+            borderRadius: "24.557px",
+            boxShadow:
+              "0px 13.9154634475708px 13.096906661987305px 0px rgba(0, 0, 0, 0.25)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src="../assets/playersIcons/2.png"
+            className="card-img-top"
+            alt="player picture"
+            style={{ height: "15rem", width: "10rem", marginTop: "30px" }}
+          />
+          <div className="card-body">
+            <a
+              href="#"
+              className="btn"
+              style={{
+                width: "12rem",
+                height: "5rem",
+                backgroundColor: "#F2F1F1",
+                borderRadius: "0 0 25px 25px ",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              Player Name
+            </a>
+          </div>
+        </div>
+        <div
+          className="card"
+          style={{
+            width: "12rem",
+            height: "20rem",
+            borderRadius: "24.557px",
+            boxShadow:
+              "0px 13.9154634475708px 13.096906661987305px 0px rgba(0, 0, 0, 0.25)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src="../assets/playersIcons/2.png"
+            className="card-img-top"
+            alt="player picture"
+            style={{ height: "15rem", width: "10rem", marginTop: "30px" }}
+          />
+          <div className="card-body">
+            <a
+              href="#"
+              className="btn"
+              style={{
+                width: "12rem",
+                height: "5rem",
+                backgroundColor: "#F2F1F1",
+                borderRadius: "0 0 25px 25px ",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              Player Name
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
