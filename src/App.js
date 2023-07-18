36993680ba37415546ec3231a619ea7f668e3b51
galadrielloves2code <126419291+galadrielloves2code@users.noreply.github.com>
2023-07-18 14:33:57 +0300

    routes were added (#38)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from './pages/home';
+import { Header } from './components/header';
+import { Schedule } from "../src/pages/schedulePage";
+import { TournamentsAccordion } from "../src/pages/tournamentsPage";
+import { RankingList } from "../src/pages/rankingPage";
+import { PlayersPage } from "../src/pages/playersPage";
 
-function App() {
+export function App() {
   return (
-    <div className="container">
-      <Home/>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/tournaments" element={<TournamentsAccordion />} />
+        <Route path="/ranking" element={<RankingList />} />
+        <Route path="/players" element={<PlayersPage />} />
+      </Routes>
+    </Router>
   );
 }
 
