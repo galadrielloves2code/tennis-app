501ed41d22e2c9545b6d98ccfd88d57c3e0f5616
galadrielloves2code <deryakarademir2@gmail.com>
2023-07-19 12:02:28 +0300

    homeMiddle component was created

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { HomeTop } from "../components/homeTop";
+import { HomeMiddle } from "../components/homeMiddle";
 
 export function Home() {
   return (
     <div>
         <HomeTop/>
+        <HomeMiddle/>
     </div>
   );
 }
