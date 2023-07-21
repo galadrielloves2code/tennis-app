91ae425f334320bde7370bfae366d1bbb97b9f33
galadrielloves2code <126419291+galadrielloves2code@users.noreply.github.com>
2023-07-21 13:18:05 +0300

    tournaments page updated (#47)

--- src/pages/tournamentsPage.jsx
+++ src/pages/tournamentsPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function TournamentsAccordion() {
   return (
-    <div className="accordion" id="tournamentsAccordion">
+    <div className="container accordion" id="tournamentsAccordion">
       <div className="accordion-item">
         <h2 className="accordion-header">
           <button
@@ -22,7 +22,7 @@ export function TournamentsAccordion() {
           data-bs-parent="#tournamentsAccordion"
         >
           <div class="accordion-body">
-            <ul style={{ listStyleType: "none", paddingLeft: 0 }}>
+            <ul style={{ paddingLeft: "20px" }}>
               <li>
                 <a
                   href="https://ausopen.com"
@@ -77,7 +77,82 @@ export function TournamentsAccordion() {
           className="accordion-collapse collapse"
           data-bs-parent="#tournamentsAccordion"
         >
-          <div class="accordion-body"></div>
+          <div class="accordion-body">
+            <ul style={{ paddingLeft: "20px" }}>
+              <li>
+                <a
+                  href="https://bnpparibasopen.com/"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Indian Wells
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.miamiopen.com/"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Miami Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://montecarlotennismasters.com/en/"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Monte Carlo Masters
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://mutuamadridopen.com/en/"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Madrid Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.internazionalibnlditalia.com/en/"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Italian Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rbccanadianopen.com/"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Canadian Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://wsopen.com/"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Cincinnati Masters
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://en.rolexshanghaimasters.com/"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Shanghai Masters
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Paris Masters
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <div className="accordion-item">
@@ -98,7 +173,114 @@ export function TournamentsAccordion() {
           className="accordion-collapse collapse"
           data-bs-parent="#tournamentsAccordion"
         >
-          <div className="accordion-body"></div>
+          <div className="accordion-body">
+            <ul style={{ paddingLeft: "20px" }}>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Rotterdam Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Rio Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Dubai Tennis Championships
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Mexican Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Barcelona Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Queen's Club Championships
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Halle Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Hamburg European Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Washington Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  China Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Japan Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Vienna Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Swiss Indoors
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <div className="accordion-item">
@@ -119,7 +301,322 @@ export function TournamentsAccordion() {
           className="accordion-collapse collapse"
           data-bs-parent="#accordionExample"
         >
-          <div className="accordion-body"></div>
+          <div className="accordion-body">
+            <ul style={{ paddingLeft: "20px" }}>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Adelaide International 1
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Tata Open Maharashtra
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Adelaide International 2
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Auckland Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Córdoba Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Open Sud de France
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Dallas Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Buenos Aires Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Delray Beach Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Open 13
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Qatar Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Chile Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  U.S. Men's Clay Court Championships
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Grand Prix Hassan II
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Estoril Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Srpska Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Bavarian International
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Geneva Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Lyon Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Stuttgart Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Stuttgart Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Rosmalen Grass Court Championships
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Mallorca Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Eastbourne International
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Infosys Hall of Fame Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Swedish Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Swiss Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Atlanta Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Croatia Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Los Cabos Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Austrian Open Kitzbühel
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Winston-Salem Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Chengdu Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Zhuhai Championships
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Astana Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  European Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Stockholm Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Moselle Open
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.rolexparismasters.com/en"
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  Tel Aviv Open
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
