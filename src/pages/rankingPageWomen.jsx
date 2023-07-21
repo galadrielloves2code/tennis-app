2abc9f75fa5d016569a84c2a11e69f32b15e7500
galadrielloves2code <deryakarademir2@gmail.com>
2023-07-21 15:13:07 +0300

    WTAInfo.json file added, rankingPage for women created

---
+++ src/pages/rankingPageWomen.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import WTAInfo from "../API/WTAInfo.json";
+
+export function RankingListWomen() {
+  return (
+    <table
+      className=" container table table-borderless table-hover"
+      style={{ marginTop: "50px" }}
+    >
+      <thead>
+        <tr className="table-success" style={{ backgroundColor: "#003500" }}>
+          <th scope="col">Rank</th>
+          <th scope="col">+/- Rank</th>
+          <th scope="col">Player</th>
+          <th scope="col">Points</th>
+          <th scope="col">+/- Points</th>
+          <th scope="col">Tourn Played</th>
+        </tr>
+      </thead>
+      <tbody>
+        {WTAInfo.tennis_players.map((player) => (
+          <tr key={player.id}>
+            <td scope="row">{player.rank}</td>
+            <td>{player["+/-rank"] !== 0 ? player["+/-rank"] : "-"}</td>
+            <td>{player.name}</td>
+            <td>{player.points}</td>
+            <td>{player["+/-points"] !== 0 ? player["+/-points"] : "-"}</td>
+            <td>{player.tournPlayed}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
