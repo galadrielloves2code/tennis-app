109c2cb26fbca73def3f9a85f5522dc703bc44ca
Derya Bağcı <126419291+galadrielloves2code@users.noreply.github.com>
2023-09-01 18:50:10 +0300

    small changes (#64)

--- src/pages/rankingPageMen.jsx
+++ src/pages/rankingPageMen.jsx
@@ -3,32 +3,34 @@ import playerInfo from "../API/playerInfo.json";
 
 export function RankingListMen() {
   return (
-    <table
-      className=" container table table-borderless table-hover"
-      style={{ marginTop: "50px" }}
-    >
-      <thead>
-        <tr className="table-success" style={{ backgroundColor: "#003500" }}>
-          <th scope="col">Rank</th>
-          <th scope="col">+/- Rank</th>
-          <th scope="col">Player</th>
-          <th scope="col">Points</th>
-          <th scope="col">+/- Points</th>
-          <th scope="col">Tourn Played</th>
-        </tr>
-      </thead>
-      <tbody>
-        {playerInfo.tennis_players.map((player) => (
-          <tr key={player.id}>
-            <td scope="row">{player.rank}</td>
-            <td>{player["+/-rank"] !== 0 ? player["+/-rank"] : "-"}</td>
-            <td>{player.name}</td>
-            <td>{player.points}</td>
-            <td>{player["+/-points"] !== 0 ? player["+/-points"] : "-"}</td>
-            <td>{player.tournPlayed}</td>
+    <div>
+      <table
+        className="container table table-borderless table-hover"
+        style={{ marginTop: "50px", width: "650px" }}
+      >
+        <thead>
+          <tr className="table-success">
+            <th scope="col">Rank</th>
+            <th scope="col">+/- Rank</th>
+            <th scope="col">Player</th>
+            <th scope="col">Points</th>
+            <th scope="col">+/- Points</th>
+            <th scope="col">Tourn Played</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {playerInfo.tennis_players.map((player) => (
+            <tr key={player.id}>
+              <td scope="row">{player.rank}</td>
+              <td>{player["+/-rank"] !== 0 ? player["+/-rank"] : "-"}</td>
+              <td>{player.name}</td>
+              <td>{player.points}</td>
+              <td>{player["+/-points"] !== 0 ? player["+/-points"] : "-"}</td>
+              <td>{player.tournPlayed}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
